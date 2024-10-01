26d395495847be049fef441af2de07d29cce1c54
Faustina-Acheampong <157317178+Faustina-Acheampong@users.noreply.github.com>
2024-10-01 23:58:25 +0200

    Created reversed.txt and wrote reversed string into it.

--- main.ts
+++ main.ts
@@ -1,9 +1,24 @@
 import { reverseString, countCharacters, compose } from "./stringUtils";
+import fs from "fs";
+import path from "path";
 
 const inputString = "Node.js enables JavaScript to be run on the server.";
 
-const reversedString = reverseString(inputString);
+const reversedInputString = reverseString(inputString);
 const characterCount = countCharacters(inputString);
 
-console.log("Reversed String: ",  reversedString);
+console.log("Reversed String: ",  reversedInputString);
 console.log("Character Count: ", characterCount);
+
+fs.writeFileSync('reversed.txt', reversedInputString);
+
+const filePath = path.join(__dirname, 'reversed.txt');
+console.log("Reversed string saved to: ", filePath);
+
+
+
+const toUpperCase = (str: string) => str.toUpperCase();
+const addExclamation = (str: string) => str + "!";
+const trim = (str: string) => str.trim();
+
+const processString = compose(addExclamation, toUpperCase, trim);
