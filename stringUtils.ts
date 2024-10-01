446486ff5037a33d1e021aa97c26bb80f7d191f2
Faustina-Acheampong <157317178+Faustina-Acheampong@users.noreply.github.com>
2024-10-02 00:24:52 +0200

    Updated countCharacters function in stringUtils.ts.

--- stringUtils.ts
+++ stringUtils.ts
@@ -3,9 +3,13 @@ export const reverseString = (str: string): string => {
     return str.split('').reverse().join('');
 };
 
+//export const countCharacters = (str: string): number => {
+    //return str.length;
+//};
+
 export const countCharacters = (str: string): number => {
-    return str.length;
-};
+    return str.replace(/\s/g, '').length;
+}
 
 // Function composition
 export const compose = <T>(...fns: ((arg: T) => T)[]) => (arg: T) =>
